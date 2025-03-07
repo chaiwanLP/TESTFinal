7a40f07c1e1e270bb886e136b5f309eca86f673b
chaiwanLP <lekrock5519@gmail.com>
2025-03-08 02:06:29 +0700

    ีupdae

--- app/includes/db.php
+++ app/includes/db.php
@@ -1,21 +1,14 @@
 <?php
+$host = 'dpg-cv5jd4i3esus73emdqn0-a.oregon-postgres.render.com';
+$dbname = 'event_management_ebim';
+$user = 'final_web_project';
+$password = 'jJTAEJ1WJf2wRV4c7TvF8owsEDEMd372';
 
-function getConnection():mysqli
-{
-    $hostname = 'dpg-cv5jd4i3esus73emdqn0-a';
-    $dbName = 'event_management_ebim';
-    $username = 'final_web_project';
-    $password = 'jJTAEJ1WJf2wRV4c7TvF8owsEDEMd372';
-    $conn = new mysqli($hostname, $username, $password, $dbName);
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
-    return $conn;
+try {
+    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
-// define('DATABASE_DIR', __DIR__ . '/../database');
-require_once DATABASE_DIR . '/users.php';
-require_once DATABASE_DIR . '/event.php';
-require_once DATABASE_DIR . '/registration.php';
-require_once DATABASE_DIR . '/event_images.php';
-require_once DATABASE_DIR . '/attendance.php';
-// 
+?>
